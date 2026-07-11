6455eb9a920fd94c8173ede7147bf6e3bc22f268
dquirogav-code <dquirogav@unal.edu.co>
2026-07-11 16:40:07 -0500

    Delete bcdtobin/bcdtobin32 directory

--- bcdtobin/bcdtobin32/comp.v
+++
@@ -1,9 +0,0 @@
-module comp (i,DONE);
-
-input wire [4:0] i;
-
-output DONE;
-
-assign DONE = (i == 5'd0);
-
-endmodule
--- bcdtobin/bcdtobin32/decs.v
+++
@@ -1,5 +0,0 @@
-module decs (B,s);
-input [31:0] B;
-output [31:0] s;
-assign s=(B<<3)+(B<<1);
-endmodule
